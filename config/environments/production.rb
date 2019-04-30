1eaec3be3ff7afa692701c67ff6855d8a1a73e47
vermilioncity <rbruehlman@gmail.com>
2019-04-29 22:07:10 -0400

    add domain to email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   #mailer
+  config.action_mailer.default_url_options = { :host => ENV['DOMAIN_NAME'] }
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
