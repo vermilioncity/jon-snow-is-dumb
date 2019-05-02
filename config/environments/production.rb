67fce5b153ecd462d5fc7390b3c297dc722898fe
vermilioncity <rbruehlman@gmail.com>
2019-05-01 20:31:29 -0400

    update domain name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
       port:                 587,
-      domain:               'gmail.com',
+      domain:               ENV['DOMAIN'],
       user_name:            ENV['EMAIL_USERNAME'],
       password:             ENV['EMAIL_PASSWORD'],
       authentication:       'plain',
