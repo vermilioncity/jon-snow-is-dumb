94176a01f0b4dce17b5dad652cef2650e64022ea
vermilioncity <rbruehlman@gmail.com>
2019-05-04 16:37:31 -0400

    removed cruft

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,11 +5,9 @@ CarrierWave.configure do |config|
       aws_access_key_id:     ENV['AWS_ACCESS_KEY'],
       aws_secret_access_key: ENV['AWS_SECRET_KEY'],
       region:                ENV['AWS_REGION'],
-      host:                  ENV['DOMAIN_NAME'],
-      endpoint:              'https://s3.example.com:8080',
       stub_responses:    Rails.env.test?
   }
   config.fog_directory  = ENV['S3_BUCKET']
   config.fog_public = true
-  
+
 end
